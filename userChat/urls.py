b0683176534690f9d7e043b071880529f52a8aef
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2025-01-14 01:28:15 +0500

    add user

--- userChat/urls.py
+++ userChat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("register/user/",RegisterUserAPIView.as_view(),name='register')
+    path("register/user/",RegisterUserAPIView.as_view(),name='register'),
+    path("login/user/",LoginUserAPIView.as_view(),name='login')
 ]
 
--- userChat/views.py
+++ userChat/views.py
@@ -1,6 +1,7 @@
 from .models import *
 import random
 from rest_framework import status
+from django.contrib.auth import authenticate
 from django.http.response import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
@@ -24,7 +25,8 @@ class RegisterUserAPIView(APIView):
         user=UserEx.objects.create(
             username=username,
             email=email,
-            password=password
+            password=password,
+            role='registered'
         )
         refresh = RefreshToken.for_user(user)
         user.save()
@@ -33,10 +35,42 @@ class RegisterUserAPIView(APIView):
                 'Success':'user has successfully added',
                 'user_id':user.id,
                 'username':user.username,
+                "role":user.role,
                 "email":user.email,
                 "JWT_accessToken" :str(refresh.access_token),
                 "JWT_refreshToken" :str(refresh),
             }
         )
         
+# ================= Login User ===============
+class LoginUserAPIView(APIView):
+    def post(self,request):
+        email=request.data.get('email')
+        password=request.data.get('password')
+        if not email and password:
+            return JsonResponse(
+                {
+                    'error':"email/password required.."
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            user = UserEx.objects.get(email=email)
+        except UserEx.DoesNotExist:
+            return JsonResponse({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
         
+        # Authenticate the user using the standard authenticate() method
+        user = authenticate(request, username=user.username, password=password)
+        if user is None:
+            return JsonResponse(
+            {'error': 'Invalid credentials'},
+             status=status.HTTP_400_BAD_REQUEST)
+        refresh=RefreshToken.for_user(user)
+        return JsonResponse(
+            {
+                'success':"Login successfully",
+                "user_id":user.id,
+                "email":user.email,
+                "JWT_accessToken" :str(refresh.access_token),
+                "JWT_refreshToken" :str(refresh),
+            }
+        )
